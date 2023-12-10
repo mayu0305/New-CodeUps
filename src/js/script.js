e5684c1022d2fd86412c1f2b97a5e5eda6fb6f92
mayu <ma.yamada1210@gmail.com>
2023-12-10 14:47:28 +0900

    FB修正1210

--- src/js/script.js
+++ src/js/script.js
@@ -96,7 +96,35 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     });
 
 
+    //スクロールアニメーション要素の取得とスピードの設定
+    var box = $('.js-scroll'),
+    speed = 500;
+
+    box.each(function(){
+        $(this).append('<div class="color"></div>')
+        var color = $(this).find($('.color')),
+        image = $(this).find('img');
+        var counter = 0;
+
+        image.css('opacity','0');
+        color.css('width','0%');
+        //inviewを使って背景色が画面に現れたら処理をする
+        color.on('inview', function(){
+            if(counter == 0){
+                    $(this).delay(200).animate({'width':'100%'},speed,function(){
+                    image.css('opacity','1');
+                    $(this).css({'left':'0' , 'right':'auto'});
+                    $(this).animate({'width':'0%'},speed);
+                })
+                counter = 1;
+            }
+        });
+    });
+
+
 }); //jQueryここまで
+
+
 
 //mvスライダー
 const firstSwiper = new Swiper(".js-fv-swiper", {
@@ -129,30 +157,7 @@ const secondSwiper = new Swiper(".js-campaign-swiper", {
 });
 
 
-//スクロールアニメーション要素の取得とスピードの設定
-var box = $('.js-scroll'),
-    speed = 500;
 
-box.each(function(){
-    $(this).append('<div class="color"></div>')
-    var color = $(this).find($('.color')),
-    image = $(this).find('img');
-    var counter = 0;
-
-    image.css('opacity','0');
-    color.css('width','0%');
-    //inviewを使って背景色が画面に現れたら処理をする
-    color.on('inview', function(){
-        if(counter == 0){
-                $(this).delay(200).animate({'width':'100%'},speed,function(){
-                image.css('opacity','1');
-                $(this).css({'left':'0' , 'right':'auto'});
-                $(this).animate({'width':'0%'},speed);
-            })
-            counter = 1;
-        }
-    });
-});
 
 //タブ
 const tabs = document.querySelectorAll('.js-tab');
@@ -220,6 +225,54 @@ tabs.forEach((tab) => {
     tab.addEventListener('keyup', tabSwitch);
 });
 //タブココまで
+
+//Informationタブはこちら
+document.addEventListener('DOMContentLoaded', function() {
+    // 初めに1つ目のタブを表示
+    showTab('tab1');
+
+    // タブがクリックされたときの処理
+    document.querySelectorAll('.js-infoTab').forEach(function(tab) {
+        tab.addEventListener('click', function(event) {
+        event.preventDefault();
+        var targetTabId = this.getAttribute('data-tab');
+        showTab(targetTabId);
+        });
+    });
+
+    // ページが読み込まれたときの処理
+    var urlParams = new URLSearchParams(window.location.search);
+    var initialTab = urlParams.get('tab');
+    if (initialTab) {
+      // URLパラメータにタブが指定されている場合はそのタブを表示
+        showTab(initialTab);
+    }
+});
+
+function showTab(tabId) {
+    // すべてのタブコンテンツを非表示にする
+    document.querySelectorAll('.js-infoContent').forEach(function(content) {
+        content.classList.remove('-active');
+    });
+
+    // すべてのタブリンクからactiveクラスを削除する
+    document.querySelectorAll('.js-infoTab').forEach(function(tabLink) {
+        tabLink.classList.remove('-active');
+    });
+
+    // 対象のタブコンテンツを表示する
+    var targetTabContent = document.getElementById(tabId);
+    if (targetTabContent) {
+        targetTabContent.classList.add('-active');
+    }
+
+    // 開いているタブに対応するタブリンクにactiveクラスを追加する
+    var activeTabLink = document.querySelector('.js-infoTab[data-tab="' + tabId + '"]');
+    if (activeTabLink) {
+    activeTabLink.classList.add('-active');
+    }
+}
+//infoタブココまで
 
 //FAQアコーディオン
 document.addEventListener('DOMContentLoaded', () => {
